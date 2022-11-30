925792d3044d3783d6a187a6707ea2e48b68c40c
AllanLeWagon <allanelsensohn@gmail.com>
2022-11-30 16:32:30 +0100

    fetch new tab and store url

--- background.js
+++ background.js
@@ -1,23 +1,65 @@
-console.log('je suis dans bck')
+// // console.log('je suis dans bck')
 
 
-chrome.tabs.query({currentWindow: true}, function(tabs) {
-  tabs.forEach(tab => {
-    let new_url = tab.url.replace(':', '%3A').replaceAll('/', '%2F')
-    fetch(`https://api.websitecarbon.com/site?url=${new_url}`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-        }
+// Every time we open a tab
+chrome.tabs.onUpdated.addListener(function(tabId, ChangeInfo, tab) {
+  if (ChangeInfo.status === 'complete') {
+  // We check if we have the user_id in the local storage
+    chrome.cookies.get({url: "http://localhost:3000", name:'signed_id'}, function(cookie) {
+      // console.log(cookie)
+      if (cookie) {
+        // we want to fetch rails with the user_id
+        visit_url = tab.url
+        console.log(visit_url)
+        fetch(`http://localhost:3000/api/v1/visits`,
+        {
+          method: 'POST',
+          headers: {
+            'X-User-Token': cookie.value,
+            'Content-Type': 'application/json'
+            },
+          body: JSON.stringify({
+            url: tab.url
+            })
+        })
+        .then(response => response.json())
+        .then(data => { console.log(data)})
+      }
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.url)
-      console.log(data.cleanerThan)
-      const stat = `<li>${data.cleanerThan}</li>`
-      document.querySelector('ul').insertAdjacentHTML('beforeend', stat);
-    })
-  });
+  }
 });
+
+// chrome.tabs.query({currentWindow: true}, function(tabs) {
+//   tabs.forEach(tab => {
+//     // let new_url = tab.url.replace(':', '%3A').replaceAll('/', '%2F')
+//     console.log(tab)
+//     fetch(`http://localhost:3000/api/v1/visits`,
+//     {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//         },
+//       body: JSON.stringify({
+//         url: tab.url
+//         })
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log(data)
+//       // console.log(data.cleanerThan)
+//       // const stat = `<li>${data.cleanerThan}</li>`
+//       // document.querySelector('ul').insertAdjacentHTML('beforeend', stat);
+//       // chrome.scripting.executeScript({
+//       //   target: {tabId: tab.id},
+//       //   files: ['content.js']
+//       // });
+//     })
+//   });
+// });
+
+
+
+// // we are goind to retreive the cookie value in the chrome local storage
+// chrome.storage.sync.get(['signed_id'], function(result) {
+//   console.log('Value currently is ' + result.signed_id);
+// });
--- popup.js
+++ popup.js
@@ -1,7 +1,12 @@
-const button = document.querySelector('button');
-
-button.addEventListener('click', () => {
-  console.log('Dashboard');
-});
-
-
+if (chrome.cookies) {
+  chrome.cookies.get({url: "http://localhost:3000", name:'signed_id'}, function(cookie) {
+    if (cookie) {
+      // we want to set in chrome storage the cookie value
+      chrome.storage.sync.set({signed_id: cookie.value}, function() {
+        console.log('Value is set to ' + cookie.value);
+      });
+    } else {
+      console.log('no cookie') // HTML sign in to rails app here
+    }
+  })
+}
