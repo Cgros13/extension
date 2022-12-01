3221c50973c2f5d359e88e9ca6e5b481aae38274
AllanLeWagon <allanelsensohn@gmail.com>
2022-12-01 14:35:17 +0100

    extension

--- background.js
+++ background.js
@@ -1,9 +1,14 @@
 
+
 chrome.tabs.onUpdated.addListener(function(tabId, ChangeInfo, tab) {
   if (ChangeInfo.status === 'complete') {
+
+
     chrome.cookies.get({url: "http://localhost:3000", name:'signed_id'}, function(cookie) {
       if (cookie) {
         visit_url = tab.url
+
+
         fetch(`http://localhost:3000/api/v1/visits`,
         {
           method: 'POST',
@@ -15,44 +20,63 @@ chrome.tabs.onUpdated.addListener(function(tabId, ChangeInfo, tab) {
             url: tab.url
             })
         })
-        .then(response => response.json())
-        .then(data => { console.log(data)})
+
+
+        // .then(response => {
+        //   console.log(response)
+        //   response.json()
+        // })
+        .then(data => {
+          if (data.status === 'done') {
+            console.log("done")
+
+            chrome.runtime.sendMessage({
+              msg: "created_visit",
+              content: {
+                subject: "success",
+                content: data["message"]
+              }
+            })
+          }
+          //erreurs eventuelles
+        })
       }
     })
   }
 });
 
-// chrome.tabs.query({currentWindow: true}, function(tabs) {
-//   tabs.forEach(tab => {
-//     // let new_url = tab.url.replace(':', '%3A').replaceAll('/', '%2F')
-//     console.log(tab)
-//     fetch(`http://localhost:3000/api/v1/visits`,
-//     {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//         },
-//       body: JSON.stringify({
-//         url: tab.url
-//         })
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log(data)
-//       // console.log(data.cleanerThan)
-//       // const stat = `<li>${data.cleanerThan}</li>`
-//       // document.querySelector('ul').insertAdjacentHTML('beforeend', stat);
-//       // chrome.scripting.executeScript({
-//       //   target: {tabId: tab.id},
-//       //   files: ['content.js']
-//       // });
-//     })
-//   });
-// });
+
+// // chrome.tabs.query({currentWindow: true}, function(tabs) {
+// //   tabs.forEach(tab => {
+// //     // let new_url = tab.url.replace(':', '%3A').replaceAll('/', '%2F')
+// //     console.log(tab)
+// //     fetch(`http://localhost:3000/api/v1/visits`,
+// //     {
+// //       method: 'POST',
+// //       headers: {
+// //         'Content-Type': 'application/json'
+// //         },
+// //       body: JSON.stringify({
+// //         url: tab.url
+// //         })
+// //     })
+// //     .then(response => response.json())
+// //     .then(data => {
+// //       console.log(data)
+// //       // console.log(data.cleanerThan)
+// //       // const stat = `<li>${data.cleanerThan}</li>`
+// //       // document.querySelector('ul').insertAdjacentHTML('beforeend', stat);
+// //       // chrome.scripting.executeScript({
+// //       //   target: {tabId: tab.id},
+// //       //   files: ['content.js']
+// //       // });
+// //     })
+// //   });
+// // });
 
 
 
-// // we are goind to retreive the cookie value in the chrome local storage
-// chrome.storage.sync.get(['signed_id'], function(result) {
-//   console.log('Value currently is ' + result.signed_id);
-// });
+// // // we are goind to retreive the cookie value in the chrome local storage
+// // chrome.storage.sync.get(['signed_id'], function(result) {
+// //   console.log('Value currently is ' + result.signed_id);
+// // });
--- popup.js
+++ popup.js
@@ -1,8 +1,12 @@
+console.log("hello popup")
+
 if (chrome.cookies) {
+console.log("cookies are enabled")
+
   chrome.cookies.get({url: "http://localhost:3000", name:'signed_id'}, function(cookie) {
     if (cookie) {
       chrome.storage.sync.set({signed_id: cookie.value}, function() {
-        // console.log('Value is set to ' + cookie.value);
+        console.log('Value is set to ' + cookie.value);
       });
     } else {
       console.log('no cookie') // HTML sign in to rails app here
@@ -10,3 +14,14 @@ if (chrome.cookies) {
   })
 }
 
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.msg === "created_visit") {
+      console.log("created visit")
+
+      console.log(request.message)
+      // let ret = JSON.parse(request)
+      // console.log(ret)
+    }
+  }
+);
