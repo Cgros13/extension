dae18f41c7d8eaa9379a224d609656c8149c91c8
Charles Gros <charles13520@gmail.com>
2022-12-05 10:21:56 +0100

    new-tab_array-of-emoji

--- content.js
+++ content.js
@@ -1,10 +1,23 @@
-function SendUrlToBackgroundPage() {
-  console.log("hello from contetn.js")
-    chrome.runtime.sendMessage({
-        message: "fetch",
-        url: window.location.href
-      });
+const retrieveAllVisitsRails = (cookie) => {
+  fetch(`http://localhost:3000/api/v1/visits`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-User-Token': cookie,
+    },
+    })
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('result').insertAdjacentHTML('beforeend', data);
+    })
+  }
+if (chrome.cookies) {
+  chrome.cookies.get({url: "http://localhost:3000", name:'signed_id'}, function(cookie) {
+    if (cookie) {
+      // we want to retrieve the url of the current tab
+      retrieveAllVisitsRails(cookie.value);
+    } else {
+      console.log('no cookie') // HTML sign in to rails app here
+    }
+  })
 }
-console.log("hello from contetn.js")
-
-SendUrlToBackgroundPage();
--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+document.getElementById('loading').style.display = 'block';
+
 const retrieveVisitsRails = (url, cookie) => {
   fetch(`http://localhost:3000/api/v1/visits?url=${url}`, {
     method: 'GET',
@@ -8,34 +10,28 @@ const retrieveVisitsRails = (url, cookie) => {
     })
     .then(response => response.json())
     .then(data => {
-      const percentage = parseInt((data[0].cleaner_than * 100));
-      const stat = `<span>${(parseInt((data[0].cleaner_than * 100)))}%</span>`
+      let last = data.length - 1
+      const percentage = parseInt((data[last].cleaner_than * 100));
+      const stat = `<span>${(parseInt((data[last].cleaner_than * 100)))}%</span>`
       if (percentage > 50) {
-        document.getElementById('response').insertAdjacentHTML('beforeend', `Cleaner than ${stat} of webpages`);
+        document.getElementById('loading').style.display = 'none';
+
+        document.getElementById('response').insertAdjacentHTML('beforeend', `Cleaner than ${stat} of webpages`); // FR
         document.getElementById('circle').dataset.percentage = percentage;
       }
       else {
-        stat2 = `<span>${100 - percentage} %</span>`
-        document.getElementById('response').insertAdjacentHTML('beforeend', `Dirtier than ${stat2} of webpages`);
+        document.getElementById('loading').style.display = 'none';
+
+        let stat2 = `<span>${100 - percentage} %</span>`
+        document.getElementById('response').insertAdjacentHTML('beforeend', `Dirtier than ${stat2} of webpages`); // FR
         document.getElementById('circle').dataset.percentage = percentage;
       }
-      // we want to store in local storage the visit id and the url
-      // chrome.storage.sync.set({results: [}, function() {
-      //   console.log('Value is set to ' + data);
-      // });
-      // chrome.runtime.sendMessage({
-      //   message: "created_visit",
-      //   cleanerThan: data.cleaner_than,
-      //   url: url
-
-      // });
     })
   }
 
 
 
 if (chrome.cookies) {
-
   chrome.cookies.get({url: "http://localhost:3000", name:'signed_id'}, function(cookie) {
     if (cookie) {
       // we want to retrieve the url of the current tab
@@ -49,63 +45,27 @@ if (chrome.cookies) {
   })
 }
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.message === "created_visit") {
-      console.log("created visit")
-      console.log(request)
-      const stat = `<span>${request.cleanerThan}</span>`
-      document.getElementById('response').insertAdjacentHTML('beforeend', stat);
-    }
-  }
-);
+
 
 // Declare an array object for array of images
+let perfectEmojis = [];
 let goodEmojis = [];
 let okEmojis = [];
 let badEmojis = [];
 
 // Push the URLs to the array of images
+perfectEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/partying-face_1f973.png');
+
 goodEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/star-struck_1f929.png');
-goodEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/smiling-face-with-heart-eyes_1f60d.png');
-goodEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/smiling-face-with-halo_1f607.png');
-goodEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/partying-face_1f973.png');
 goodEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/leaf-fluttering-in-wind_1f343.png');
 goodEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/seedling_1f331.png');
-goodEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/growing-heart_1f497.png');
-goodEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/heart-hands_1faf6.png');
-goodEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/hugging-face_1f917.png');
-goodEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/partying-face_1f973.png');
-goodEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/thumbs-up_1f44d.png');
-goodEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/folded-hands_1f64f.png');
 
-
-okEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/smiling-face-with-smiling-eyes_1f60a.png');
 okEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/slightly-smiling-face_1f642.png');
 okEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/neutral-face_1f610.png');
 okEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/face-without-mouth_1f636.png');
 
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/biohazard_2623-fe0f.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/350/radioactive_2622-fe0f.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/melting-face_1fae0.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/smiling-face-with-tear_1f972.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/face-with-peeking-eye_1fae3.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/grimacing-face_1f62c.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/face-with-medical-mask_1f637.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/face-with-thermometer_1f912.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/face-with-head-bandage_1f915.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/sneezing-face_1f927.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/exploding-head_1f92f.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/hot-face_1f975.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/cold-face_1f976.png');
 badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/woozy-face_1f974.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/frowning-face_2639-fe0f.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/worried-face_1f61f.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/slightly-frowning-face_1f641.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/fearful-face_1f628.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/anxious-face-with-sweat_1f630.png');
 badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/face-screaming-in-fear_1f631.png');
-badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/mending-heart_2764-fe0f-200d-1fa79.png');
 badEmojis.push('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/thumbs-down_1f44e.png');
 
 
@@ -119,21 +79,29 @@ setTimeout(function(){
     let val = Number(circle.getAttribute('data-percentage'));
     let bar = circle.querySelectorAll('.c-progress-circle__bar')[0];
 
-    if (val < 50) {
-      bar.style.stroke = "#6A040F"
+    if (val === 1) {
+      bar.style.stroke = "#d00000";
+      emoji.src = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/face-with-thermometer_1f912.png";
+    } else if (val < 30) {
+      bar.style.stroke = "#d00000"
       //emoji bad
       let randomIndexBad = Math.floor(Math.random() * badEmojis.length);
       emoji.src = badEmojis[randomIndexBad];
-    } else if (val < 51) {
+    } else if (val < 60) {
       //emoji moyen
-      bar.style.stroke = "#F9844A"
+      bar.style.stroke = "#e3d400"
       let randomIndexOk = Math.floor(Math.random() * okEmojis.length);
       emoji.src = okEmojis[randomIndexOk];
-    }else {
-      // emoji cool
-      bar.style.stroke = "#31572C"
+    } else if (val < 90) {
+      //emoji bon
+      bar.style.stroke = "#31c200"
       let randomIndexGood = Math.floor(Math.random() * goodEmojis.length);
       emoji.src = goodEmojis[randomIndexGood];
+    } else {
+      // emoji perfect
+      bar.style.stroke = "#00ffee"
+      let randomIndexGood = Math.floor(Math.random() * goodEmojis.length);
+      emoji.src = perfectEmojis[randomIndexGood];
     }
 
     if (isNaN(val)) {
