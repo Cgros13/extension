513778d8b80e5a129f53c5fc23dc912514a7b01a
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-12-13 16:58:16 +0100

    refacto, indentation plus stop fetching localhost

--- background.js
+++ background.js
@@ -1,10 +1,16 @@
 const tabs = [];
 
-chrome.tabs.onUpdated.addListener(function(tabId, ChangeInfo, tab) {
-  if (tab.url?.startsWith("chrome://") || tab.url?.startsWith("https://www.google.com/search?") || tab.url?.startsWith("https://www.c0-de.tech") || tab.url?.startsWith("https://c0-de.tech")) {
+chrome.tabs.onUpdated.addListener(function (tabId, ChangeInfo, tab) {
+  if (
+    tab.url?.startsWith("chrome://") ||
+    tab.url?.startsWith("https://www.google.com/search?") ||
+    tab.url?.startsWith("https://www.c0-de.tech") ||
+    tab.url?.startsWith("https://c0-de.tech") ||
+    tab.url?.startsWith("http://localhost:3000")
+  ) {
     return undefined;
   } else {
-    if (ChangeInfo.status === 'complete') {
+    if (ChangeInfo.status === "complete") {
       // find if tab exists in tabs
       const foundTab = tabs.find((tabItem) => {
         return tabItem.tabId === tabId;
@@ -12,13 +18,12 @@ chrome.tabs.onUpdated.addListener(function(tabId, ChangeInfo, tab) {
 
       // if yes, update
       if (foundTab) {
-        sendEndTimeToRails(foundTab)
-        foundTab.url = tab.url
+        sendEndTimeToRails(foundTab);
+        foundTab.url = tab.url;
         foundTab.visitId = undefined;
         // if no, add
       }
-      sendVisitToRails(tab.url)
-      .then((tabData) => {
+      sendVisitToRails(tab.url).then((tabData) => {
         console.log(tabData);
         const tabObject = {
           tabId: tabId,
@@ -26,12 +31,12 @@ chrome.tabs.onUpdated.addListener(function(tabId, ChangeInfo, tab) {
           visitId: tabData.id,
         };
         tabs.push(tabObject);
-      })
+      });
     }
   }
 });
 
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
   console.log(removeInfo);
   if (removeInfo) {
     console.log("discarded");
@@ -57,60 +62,61 @@ const sendVisitToRails = (url) => {
     chrome.cookies.get(
       {
         url: "https://www.c0-de.tech/",
-        name:'signed_id'
+        name: "signed_id",
       },
-      function(cookie) {
+      function (cookie) {
         if (cookie) {
-          fetch('https://www.c0-de.tech/api/v1/visits', {
-            method: 'POST',
+          fetch("https://www.c0-de.tech/api/v1/visits", {
+            method: "POST",
             headers: {
-              'Content-Type': 'application/json',
-              'X-User-Token': cookie.value,
+              "Content-Type": "application/json",
+              "X-User-Token": cookie.value,
             },
             body: JSON.stringify({
               visit: {
                 url: url,
-              }
-            })
+              },
+            }),
           })
-            .then(response => response.json())
-            .then(data => {
+            .then((response) => response.json())
+            .then((data) => {
               console.log("je send le message avant runtime");
-              chrome.runtime.sendMessage({message: "update", data: data});
+              chrome.runtime.sendMessage({ message: "update", data: data });
               console.log("je send le message apres runtime");
-              resolve(data)
-            })
+              resolve(data);
+            });
         }
       }
-    )
-  })
-}
+    );
+  });
+};
 
 const sendEndTimeToRails = (tab) => {
   return new Promise((resolve, reject) => {
     chrome.cookies.get(
-      {url: "https://www.c0-de.tech", name:'signed_id'},
-      function(cookie) {
+      { url: "https://www.c0-de.tech", name: "signed_id" },
+      function (cookie) {
         if (cookie) {
-        fetch(`https://www.c0-de.tech/api/v1/visits/${tab.visitId}`, {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-User-Token': cookie.value,
-          },
-          body: JSON.stringify({
-            visit: {
-              end_time: Date.now(),
-            }
+          fetch(`https://www.c0-de.tech/api/v1/visits/${tab.visitId}`, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+              "X-User-Token": cookie.value,
+            },
+            body: JSON.stringify({
+              visit: {
+                end_time: Date.now(),
+              },
+            }),
           })
-        })
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-          resolve(data);
-          console.log(end_time)
-        })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data);
+              resolve(data);
+              console.log(end_time);
+            });
+        }
       }
-    })
-  })
+    );
+  });
 };
